8ba79273cb5e71d10459830b73a1876b6342630c
longok <huynhphuoclong2@gmail.com>
2022-05-31 15:46:11 +0700

    sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 
     def index
-
+        @users = User.all
     end
 
     def show
-
+        @user = User.find_by id: params[:id]
     end
 
     def new
@@ -13,10 +13,19 @@ class UsersController < ApplicationController
     end
 
     def create
-
+        @user = User.new user_params
+        if @user.save
+            flash[:info] = "Crate account successfully"
+            redirect_to root_path
+        else
+            render :new
+        end
     end
 
-
+    private
+    def user_params
+        params.require(:user).permit :name, :email, :password, :password_confirmation
+    end
 
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
+
+    validates :name, presence: true
+    validates :password, presence: true, length: { minimum: 6 }
+
+    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: true, length: {minimum: 4}, format: {with: EMAIL_REGEX}
+
+    has_secure_password
 end
