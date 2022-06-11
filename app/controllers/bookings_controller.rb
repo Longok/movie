88f5d1ee4130900414320cd164d9604a97d989d4
longok <huynhphuoclong2@gmail.com>
2022-06-11 15:31:58 +0700

    index booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,31 @@
 class BookingsController < ApplicationController
+
+    def index
+        @bookings = Booking.all.order('id DESC')
+    end
+
+    def new
+        @booking = Booking.new
+    end
+
+
+    def create
+        @booking = Booking.new booking_params
+        if @booking.save
+            flash[:info] = "Booking successfully"
+            redirect_to @booking
+        else
+            render :new
+        end
+    end
+
+    def show
+        @booking = Booking.find params[:id]
+
+    end
+
+    private
+    def booking_params
+        params.require(:booking).permit :seat_id, :cinema_id, :showtime_id
+    end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :cinema
   belongs_to :seat
   belongs_to :showtime
+
+
+  
 end
